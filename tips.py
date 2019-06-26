6213a62b4e5ae9dc2d2f061addaf403c089ed460
Ahmed Shahin <ahmedhassan9922@gmail.com>
2019-06-26 11:50:46 +0400

    Update tips.py

--- tips.py
+++ tips.py
@@ -6,3 +6,6 @@ model.load_state_dict(state)
 
 # to load part of existing model to your net
 net.load_state_dict(saved, strict = False)
+
+# print no of trainiable parameters
+print("No of parameters: ", sum(p.numel() for p in net.parameters() if p.requires_grad))
